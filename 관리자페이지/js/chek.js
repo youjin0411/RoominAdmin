51514f36fb3b8ae09b176bb6ee7e4aeb90866ab6
youjin <w2117@e-mirim.hs.kr>
2023-06-09 15:12:44 +0900

    프론트

--- 관리자페이지/js/chek.js
+++ 관리자페이지/js/chek.js
@@ -1,5 +1,25 @@
-const cho = document.getElementById(choice);
+// 모달 열기 버튼을 가져옵니다.
+const modalButton = document.getElementById("detail");
 
-cho.addEventListener('click', function() {
-    
-})
+// 모달 창을 가져옵니다.
+const modal = document.getElementById("myModal");
+
+// 모달 창을 닫는 요소를 가져옵니다.
+const closeBtn = document.getElementsByClassName("close")[0];
+
+// 모달 열기 버튼 클릭 시 모달 창을 보이게 합니다.
+modalButton.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+// 모달 닫기 버튼 클릭 시 모달 창을 숨깁니다.
+closeBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+// 모달 외부 클릭 시 모달 창을 숨깁니다.
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
